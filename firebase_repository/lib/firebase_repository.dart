35f63b2c730429cb7ff00da8a2bf015fde0518df
Robert Zeng <jianze_zeng@foxitsoftware.cn>
2020-07-24 19:31:24 +0800

    2020year07month24day

--- firebase_repository/lib/firebase_repository.dart
+++ firebase_repository/lib/firebase_repository.dart
@@ -2,6 +2,8 @@ library firebase_repository;
 export 'src/firebase_products_repository.dart';
 export 'src/models/models.dart';
 export 'src/products_repository.dart';
+export 'src/models/models.dart';
+export 'src/products_repository.dart';
 
 /// A Calculator.
 class Calculator {
