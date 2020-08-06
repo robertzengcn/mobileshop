2bc61a16300f17fe1c9f93d5b278cbd535cb0889
Robert Zeng <jianze_zeng@foxitsoftware.cn>
2020-08-06 19:32:42 +0800

    2020year08month06day

--- lib/UI/widgets/home_image_slide.dart
+++ lib/UI/widgets/home_image_slide.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:treva_shop_flutter/blocs/blocs.dart';
+import 'package:treva_shop_flutter/Library/carousel_pro/carousel_pro.dart';
 
 class HomeImageSlide extends StatelessWidget {
   HomeImageSlide({Key key}) : super(key: key);
@@ -13,8 +14,43 @@ class HomeImageSlide extends StatelessWidget {
             if(state is HomeImagesliderLoading){// is loading
               return Center(child: CircularProgressIndicator());
             }else if(state is HomeImagesliderloaded){
-
+              final imagesliders = state.filteredImagesliders;
+              return Container(
+                height: 182.0,
+                child: new Carousel(
+                    boxFit: BoxFit.cover,
+                    dotColor: Color(0xFF6991C7).withOpacity(0.8),
+                    dotSize: 5.5,
+                    dotSpacing: 16.0,
+                    dotBgColor: Colors.transparent,
+                    showIndicator: true,
+                    overlayShadow: true,
+                    overlayShadowColors: Colors.white.withOpacity(0.9),
+                    overlayShadowSize: 0.9,
+                    images: imagesliders?.map((i) {
+                      return NetworkImage(i.url);
+                    })?.toList() ??
+                        []),
+              );
             }
         });
   }
 }
+/// ImageSlider in header
+    var imageSliderview = Container(
+      height: 182.0,
+      child: new Carousel(
+          boxFit: BoxFit.cover,
+          dotColor: Color(0xFF6991C7).withOpacity(0.8),
+          dotSize: 5.5,
+          dotSpacing: 16.0,
+          dotBgColor: Colors.transparent,
+          showIndicator: true,
+          overlayShadow: true,
+          overlayShadowColors: Colors.white.withOpacity(0.9),
+          overlayShadowSize: 0.9,
+          images: imagesliders?.map((i) {
+                return NetworkImage(i.url);
+              })?.toList() ??
+              []),
+    );
